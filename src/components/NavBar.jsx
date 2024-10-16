414a88361615d435ec27fe3602b1f80a100af8ea
GROOT <nafisfoisal25@gmail.com>
2024-10-16 20:02:22 +0600

    filter and search feature added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,12 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const NavBar = () => {
 
     const [visible,setVisible]=useState(false);
+    const {setShowSearch} = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -30,7 +32,7 @@ const NavBar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img /*onClick={()=>setShowSearch(true)} */src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img onClick={()=> setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
         <div className="group relative">
          <Link to='/login'> <img
             src={assets.profile_icon}
